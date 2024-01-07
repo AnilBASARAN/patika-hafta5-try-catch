9ea605bb3de1bae151930303e0b7e882ba4ff0c2
Anıl Başaran <35750219+AnilBASARAN@users.noreply.github.com>
2024-01-07 20:42:34 +0300

    try and catch metodu
    arrayden indexi belli bir elemanı almak için kullanılan metodda hata durumunu yönetmek için try and catch kullanıyoruz

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+
+                int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+                int index;
+
+        System.out.println(Arrays.toString(array));
+        System.out.print("Array'den eleman seçmek için bir sayı girin:   ");
+        index = input.nextInt();
+
+                try {
+                    // Hata yakalama try bloğu
+                    int result = getElementIndex(array, index);
+                    System.out.println("Dizinin " + index + ". indeksindeki eleman: " + result);
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    // Hata durumunda mesajı yazdırma
+                    System.out.println("Hata mesajı: Belirtilen index dizinin boyutunun dışında!");
+                }
+            }
+
+            // Belirli bir indeksteki array elemanını döndüren metot
+            public static int getElementIndex(int[] array, int index) {
+
+                return array[index];
+            }
+        }
---
+++ out/production/patika-hafta5-try-catch/README.md
@@ -0,0 +1,2 @@
+# patika-hafta5-try-catch
+10 elemanlı tek boyutlu bir dizi içerisinde kullanıcıdan alınan indeksteki elemanı döndüren bir Java metodu yazın.
